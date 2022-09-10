cefc8fbffea545a9593e8235151d703a5b4eaab9
AnthonymStrickland <101576799+AnthonymStrickland@users.noreply.github.com>
2022-09-10 11:38:18 -0500

    Add files via upload
    this is my final project for the wash u boot camp this is a striped-down command script with minimal code input

---
+++ bootcamp.py
@@ -0,0 +1,13 @@
+# this is the script for my final project this will install docker wireshark
+# and create a back up of the system
+
+#!
+sudo apt install wireshark -y
+
+sudo apt install docker.io -y
+
+sudo apt install dump -y
+
+ history  > history.txt
+
+
